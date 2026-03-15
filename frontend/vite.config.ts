156f04a071c531a56459231a4a1c7e99b18635a4
Kristian Anton Hedegaard <kristianantonhedegaard@gmail.com>
2026-03-15 10:49:27 +0100

    add domain as host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
         react(),
         babel({ presets: [reactCompilerPreset()] })
     ],
+    server: {
+    allowedHosts: [
+      "algo.froemosen.dk"
+    ]
+  }
 })
